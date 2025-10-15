964dc09b128c043266ea6a0a22f7ad7551cb3c26
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2025-10-15 15:01:46 +0100

    added image support for some domain

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	images: {
+		domains: [
+			'encrypted-tbn0.gstatic.com',
+			'unsplash.com',
+			'images.unsplash.com',
+			'cdn.pixabay.com',
+			'res.cloudinary.com',
+			'lh3.googleusercontent.com',
+			'avatars.githubusercontent.com',
+			'media.licdn.com',
+			'www.gravatar.com',
+			'canva.com',
+		],
+	},
+};
